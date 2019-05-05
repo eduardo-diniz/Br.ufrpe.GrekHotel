c5b55132eeb65ea533c1a415cf48efa47e15a380
Rodrigo-Tibirica <47817462+Rodrigo-Tibirica@users.noreply.github.com>
2019-05-05 15:35:12 -0300

    Update RepServicos.java

--- GrekHotel/src/br/ufrpe/GrekHotel/Dados/RepServicos.java
+++ GrekHotel/src/br/ufrpe/GrekHotel/Dados/RepServicos.java
@@ -1,14 +1,58 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.ufrpe.GrekHotel.Dados;
 
-/**
- *
- * @author fight
- */
+import java.util.ArrayList;
+
+import br.ufrpe.GrekHotel.beans.*;
+
 public class RepServicos {
-    
+	 private ArrayList<Servico> quartos;
+	 private static RepServicos instance;
+	 
+	 private RepServicos() {
+		 
+		 quartos = new ArrayList();
+	 }
+	 public static RepServicos getInstance(){
+			if (instance == null){
+				instance = new RepServicos(); 
+				
+			}
+			return instance;
+		}
+	 
+	 
+	 public void cadastrar(Servico servico) {
+		 if(!this.quartos.contains(servico)) {
+			 quartos.add(servico);
+			 
+		 }	 
+	 }
+	 public Servico procurar(String descricao) {
+		Servico resultado = null;
+	           	for(Servico i : quartos) {
+				if (i.getDescricao().equals(descricao)){
+					
+					resultado = i;
+				}
+			
+	
+	     
+	           	}
+				return resultado;
+			 }
+	 
+	 public boolean remove(Servico servico){
+			boolean resultado = false;
+				 
+				 if(this.quartos.contains(servico)){ 
+					
+					resultado = true;
+					quartos.remove(servico);
+			}
+			return resultado;
+				
+			}
+	     
+
+
 }
