d998ced09ab1638210afa8a32ac5c8574f87f4c1
rodri <rodri@LAPTOP-M8QCG0UP>
2019-05-05 04:01:20 -0300

    Commit Visita, Reserva, RepRepositorio e ControladorRepositorio

--- GrekHotel/src/br/ufrpe/GrekHotel/Negocio/ControladorReservas.java
+++ GrekHotel/src/br/ufrpe/GrekHotel/Negocio/ControladorReservas.java
@@ -4,11 +4,54 @@
  * and open the template in the editor.
  */
 package br.ufrpe.GrekHotel.Negocio;
+import java.time.LocalDate;
 
-/**
- *
- * @author fight
- */
 public class ControladorReservas {
-    
+
+private RepReserva reserva;
+private static ControladorReservas instance;  
+
+private  ControladorReservas(){
+	this.reserva = RepReserva.getInstance();
+
+}
+public static ControladorReservas getInstance(){
+	if (instance == null){
+		instance = new ControladorReservas(); 
+	}
+	return instance;
+}
+public void Reservar(Reserva reserva){ 
+	
+	this.RepReserva.cadastrar(reserva);
+}
+
+public Reserva procurarReserva(Quarto quarto){
+	 this.RepReserva.procurar(quarto);
+}
+
+
+public Reserva procurarReserva(Cliente cliente){
+	 this.RepReserva.procurar(cliente);
+}
+
+public void atualizarReserva(Reserva desatualizado, Reserva atualizado){
+	
+	
+}
+
+public boolean cancelarReserva(Reserva reserva){
+this.RepReserva.remove(reserva);
+	
+}
+public void checkIn(Reserva reserva){
+     reserva.getVisita().setCheckIn(LocalDateTime.now());
+
+}
+
+public void checkOut(Reserva reserva){
+	reserva.getVisita().setCheckOut(LocalDateTime.now());
+	
+}
+
 }
