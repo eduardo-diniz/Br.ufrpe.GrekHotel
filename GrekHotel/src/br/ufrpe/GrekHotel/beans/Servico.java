016e02f5ae1cbc8fb2771bd156f2e318f7babf59
Rodrigo-Tibirica <47817462+Rodrigo-Tibirica@users.noreply.github.com>
2019-05-05 15:34:16 -0300

    Update Servico.java

--- GrekHotel/src/br/ufrpe/GrekHotel/beans/Servico.java
+++ GrekHotel/src/br/ufrpe/GrekHotel/beans/Servico.java
@@ -1,14 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.ufrpe.GrekHotel.beans;
 
-/**
- *
- * @author fight
- */
+
 public class Servico {
-    
+	private String descricao;
+	private double custo;
+	
+	
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public double getCusto() {
+		return custo;
+	}
+	
+	public void setCusto(double custo) {
+		this.custo = custo;
+	}
+	
+	public boolean equals(Object obj) {
+		
+
+		boolean resultado = false;
+
+		if(obj instanceof Servico){
+			Servico b =(Servico) obj;
+
+			if(this.descricao.equals(b.getDescricao())){
+				resultado = true;
+			}
+			
+	}
+		return resultado;
+	}
+	
+
 }
+
+
+
+
+
