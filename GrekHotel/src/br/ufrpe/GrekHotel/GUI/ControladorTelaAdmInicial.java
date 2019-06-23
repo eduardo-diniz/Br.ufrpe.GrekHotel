ccaa4e416261f301b6363282d4c3d8fa579e4144
Eduardo Diniz <eduardogomesdiniz42@gmail.com>
2019-06-23 16:17:26 -0300

    Criando Controladores de tela(TelaAdmInicial) 0.7.7 (@Eduardo-Diniz)

---
+++ GrekHotel/src/br/ufrpe/GrekHotel/GUI/ControladorTelaAdmInicial.java
@@ -0,0 +1,35 @@
+package br.ufrpe.GrekHotel.GUI;
+
+import br.ufrpe.GrekHotel.Negocio.Sistema;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class ControladorTelaAdmInicial {
+
+    @FXML
+    private Button btnSair;
+
+    @FXML
+    private Button btnTelaReserva;
+
+    @FXML
+    private Button btnTelaQrtServ;
+
+    @FXML
+    private Button btnTelaFuncionario;
+
+    @FXML
+    private Label lblNomeAdm;
+    
+    
+    public void initialize() {
+
+ 
+
+    }
+
+}
+
